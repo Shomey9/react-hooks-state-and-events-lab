324137e4f9210750e2fd210ff952b8ff52aa3b43
Muhammad Rendusara <hadyanrendusara@yahoo.com>
2021-12-03 07:50:47 -0600

    commit tree ac491a183be7e571f2c7f4fae7907d1b7039bc20

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
+  const [darkIsOn, darkSetOn] = useState(false);
+  function handleDark() {
+    darkSetOn((darkIsOn) => !darkIsOn)
+  }
 
+  const [addToCart, cartsetOn] = useState(false);
+  function handleCart() {
+    cartsetOn((addToCart) => !addToCart)
+  } 
+  
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  const appClass = darkIsOn ? "App dark" : "App light"
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={handleDark}>{darkIsOn ? "Dark Mode" : "Light Mode"}</button>
       </header>
       <ShoppingList items={itemData} />
+        
     </div>
   );
 }
